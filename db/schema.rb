7fbf95125999352e2aff7bff0954b4dd548601cc
musiccity365 <m_sackstein@yahoo.com>
2021-01-01 23:06:31 -0500

    initial db migrate & seed

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20210102025618) do
+
+  create_table "phones", force: :cascade do |t|
+    t.string "name"
+    t.string "model"
+    t.string "color"
+    t.string "vintage"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "username"
+    t.string "password"
+  end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -3,3 +3,7 @@ Phone.create(name: "Kellogg/ITT", model: "554 Rotary Wall Telephone", vintage: "
 Phone.create(name: "Northern Telecom", model: "500 Rotary Desk Telephone", vintage: "1970-present")
 Phone.create(name: "AT&T", model: "500 Rotary Desk Telephone", vintage: "1970-present")
 Phone.create(name: "Western Electric", model: "302 Rotary Desk Telephone", vintage: "1940-69")
+
+User.create(:name => "Michael Sackstein", :username => "musiccity365", :password => "Password1",)
+User.create(:name => "John Smith", :username => "vwrabbit245", :password => "Password2",)
+User.create(:name => "Jane Doe", :username => "phonelover123", :password => "Password3",)
