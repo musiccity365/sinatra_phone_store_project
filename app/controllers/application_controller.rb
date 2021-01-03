94462fa1b18936c2fbe28f60a14aaa4b3313e4e3
musiccity365 <m_sackstein@yahoo.com>
2021-01-03 03:36:37 -0500

    --- Auto-Git Commit ---

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -40,23 +40,6 @@ class ApplicationController < Sinatra::Base
   end
 
 
-  get '/login' do
-    erb :"/login"
-  end
-
-  post '/login' do
-    user = User.find_by(username: params[:user][:username])
-    binding.pry
-    if user
-      # log them in
-      # redirect
-    else
-
-    end
-  end
-
-  get '/logout' do
-    session.clear
-  end
+  
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,13 +18,35 @@ class UsersController < ApplicationController
     user = User.new(params[:user])
     if user.save
       # redirect
-      session[:user_id] = user_id # if true, then login user
+      session[:user_id] = user.id # if true, then login user
       redirect to "/users"
     else      
       # binding.pry
       @errors = user.errors.full_messages.join(" - ")
       erb :"users/new" # if false, then render instead of redirect so that you don't loose access to the error instances
     end
+  end
+
+  get '/login' do
+    erb :"users/login"
+  end
+
+  post '/login' do
+    user = User.find_by(username: params[:user][:username])
+    # binding.pry
+    if user && user.authenticate(params[:user][:password])
+      # log them in
+      session[:user_id] = user.id
+      # redirect
+      redirect to "/users"
+    else
+      redirect to "/login"
+    end
+  end
+
+  get '/logout' do
+    session.clear
+    redirect to '/login'
   end
 
   # get '/users' do #index - loads all the users
